f4338d4253a1825f51c2bd0c7f8086d79dc7ca72
name <gramkracker88@gmail.com>
2020-09-02 15:09:06 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// Parent Element
+
+const heading = document.querySelector('h2');
+// console.log(heading.parentElement.parentElement.parentElement);// displays parents of html
+const parent = heading.parentElement;
+parent.style.color = 'red';
